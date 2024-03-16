37930554351a321c3cf6eb1e59815f5955e0f067
felix30526 <163672953+felix30526@users.noreply.github.com>
2024-03-16 12:31:44 -0500

    Add files via upload

---
+++ yo.py
@@ -0,0 +1,91 @@
+"""edad = 25
+if edad >= 18:
+    print(f"jubilate ome , tu edad es {edad}")
+else:
+    print(f"sos menor de edad bro ,tu edad es {edad}") #
+"""
+
+
+"""peso = int(input("decime tu peso plis (kg)"))
+estatura = int(input("rotame tu estatura (cm)"))
+print("su indice de masa corporal es", peso/estatura * estatura)
+"""
+
+
+
+"""def suma(a, b):
+    return a + b
+    
+def resta(a, b):
+    return a - b
+    
+def multiplicacion(a,b):
+    return a * b
+
+def division (a,b):
+    return a / b
+
+
+resultado1 = resta(4,6)
+resultado2 = suma(15,18)
+resultado3 = multiplicacion(4,5)
+resultado4 = division(5,2)
+
+print(f"resta: {resultado1}, suma:{resultado2}, multiplicacion:{resultado3}, division:{resultado4 }")
+
+"""
+"""edad = int(input("coloque su edad:"))
+
+if edad <= 13:
+      print("vos sos un infante")
+
+elif edad >= 14 and edad <18:
+      print("vos sos un adolencente")
+
+elif edad > 18 and edad <100:
+      print("vos sos un adulto")
+
+elif edad > 100:
+    print("vos sos un muerto")
+"""
+
+"""EstadosUnidos = 900 * 18
+Canadá = 800 * 16
+Europa = 950 * 20
+Restodelmundo = 1250 * 16
+
+if EstadosUnidos > 900 * 15:
+    print ("tiene descuento del 20%")
+
+if Canadá > 800 * 15:
+    print ("tiene descuento del 20%")
+
+if Europa > 950 * 15:
+    print ("tiene descuento del 20%")
+
+if Restodelmundo > 1250 * 15:
+    print ("tiene descuento del 20%")"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
